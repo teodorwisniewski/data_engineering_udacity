b9e17c4ed03a148d7368e6f260cf3c62d1a5048b
Teodor Wisniewski <teodor.wisniewski@altmansolon.com>
2021-12-13 12:58:50 +0100

    spelling error in the userAgent column during insertion to the songplay table

--- 3_Data_Modeling_With_Postgres/etl.py
+++ 3_Data_Modeling_With_Postgres/etl.py
@@ -71,7 +71,7 @@ def process_log_file(cur, filepath):
 
         # insert songplay record
         songplay_data = (index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location,
-                         row.userAgen)
+                         row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
