f496034990e9927e839ebee1a80f9221d4dbf0e0
Teodor Wisniewski <teodor.wisniewski@altmansolon.com>
2022-08-27 14:04:19 +0200

    creating tables

---
+++ warehouse_project_file/sql_queries.py
@@ -0,0 +1,167 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+# DROP TABLES
+
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+
+staging_events_table_create= ("""
+        CREATE TABLE staging_events
+        (
+            event_id INT IDENTITY(0,1),
+            artist_name VARCHAR(255),
+            auth VARCHAR(50),
+            user_first_name VARCHAR(255),
+            user_gender  VARCHAR(1),
+            item_in_session	INTEGER,
+            user_last_name VARCHAR(255),
+            song_length	DOUBLE PRECISION, 
+            user_level VARCHAR(50),
+            location VARCHAR(255),	
+            method VARCHAR(25),
+            page VARCHAR(35),	
+            registration VARCHAR(50),	
+            session_id	BIGINT,
+            song_title VARCHAR(255),
+            status INTEGER, 
+            ts VARCHAR(50),
+            user_agent TEXT,	
+            user_id VARCHAR(100),
+            PRIMARY KEY (event_id)
+        );
+""")
+
+staging_songs_table_create = ("""
+CREATE TABLE staging_songs
+(
+    song_id VARCHAR(100),
+    num_songs INTEGER,
+    artist_id VARCHAR(100),
+    artist_latitude DOUBLE PRECISION,
+    artist_longitude DOUBLE PRECISION,
+    artist_location VARCHAR(255),
+    artist_name VARCHAR(255),
+    title VARCHAR(255),
+    duration DOUBLE PRECISION,
+    year INTEGER,
+    PRIMARY KEY (song_id)
+    );
+""")
+
+songplay_table_create = ("""
+CREATE TABLE songplays
+(
+    songplay_id INT IDENTITY(0,1),
+    start_time TIMESTAMP,
+    user_id VARCHAR(50),
+    level VARCHAR(50),
+    song_id VARCHAR(100),
+    artist_id VARCHAR(100),
+    session_id BIGINT,
+    location VARCHAR(255),
+    user_agent TEXT,
+    PRIMARY KEY (songplay_id)
+    );
+""")
+
+user_table_create = ("""
+CREATE TABLE users
+(
+    user_id VARCHAR,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    gender VARCHAR(1),
+    level VARCHAR(50),
+    PRIMARY KEY (user_id)
+    );
+""")
+
+song_table_create = ("""
+CREATE TABLE songs
+(
+    song_id VARCHAR(100),
+    title VARCHAR(255),
+    artist_id VARCHAR(100) NOT NULL,
+    year INTEGER,
+    duration DOUBLE PRECISION,
+    PRIMARY KEY (song_id)
+    );
+""")
+
+artist_table_create = ("""
+CREATE TABLE artists
+(
+    artist_id VARCHAR(100),
+    name VARCHAR(255),
+    location VARCHAR(255),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    PRIMARY KEY (artist_id)
+    );
+""")
+
+time_table_create = ("""
+CREATE TABLE time
+(
+    start_time TIMESTAMP,
+    hour INTEGER,
+    day INTEGER,
+    week INTEGER,
+    month INTEGER,
+    year INTEGER,
+    weekday INTEGER,
+    PRIMARY KEY (start_time)
+    );
+""")
+
+# STAGING TABLES
+
+staging_events_copy = (F"""
+                          COPY staging_events 
+                          FROM {config.get('S3','LOG_DATA')}
+                          iam_role {config.get('IAM_ROLE', 'ARN')}
+                          json {config.get('S3','LOG_JSONPATH')};
+""").format()
+
+staging_songs_copy = (f"""
+                          COPY staging_songs 
+                          FROM {config.get('S3','SONG_DATA')} 
+                          iam_role {config.get('IAM_ROLE', 'ARN')}
+                          json 'auto';
+""").format()
+
+# FINAL TABLES
+
+songplay_table_insert = ("""
+""")
+
+user_table_insert = ("""
+""")
+
+song_table_insert = ("""
+""")
+
+artist_table_insert = ("""
+""")
+
+time_table_insert = ("""
+""")
+
+# QUERY LISTS
+
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
